8d3116efc5dea4e820d52d94110dd1b84a6b69b5
Lucaspaixao17 <lukinhas0617@gmail.com>
2026-08-21 16:24:10 -0300

    Q5.php

--- Q5.php
+++ Q5.php
@@ -4,6 +4,6 @@ $n1 = readline("Digite um número: ");
 $dobro = ($n1 *2);
 $triplo = ($n1 *3);
 
-echo "Dobro: ".$dobro;
-echo "Triplo: ".$triplo;
+echo "Dobro: \n".$dobro;
+echo "\nTriplo: \n".$triplo;
 ?>
